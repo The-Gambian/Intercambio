a3178104f7a80189a9bd65ad403b782ca3572e74
The-Gambian <kahmalick@gmail.com>
2025-06-09 17:24:47 +0100

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pan-red': '#BE0002',
+        'pan-black': '#000000',
+        'pan-green': '#008751',
+        'pan-gold': '#FDBD00',
+        'kente-purple': '#800080',
+        'kente-blue': '#000080',
+        'earth-red': '#8B0000',
+        'savanna-brown': '#8B4513',
+        primary: {
+          DEFAULT: '#BE0002',
+          dark: '#8B0000',
+          light: '#FF1A1A'
+        },
+        accent: {
+          DEFAULT: '#FDBD00',
+          dark: '#CC9900',
+          light: '#FFD700'
+        }
+      }
+    },
   },
   plugins: [],
 };
