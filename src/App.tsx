019edd9603327b63969bb0e491d9e26348351baa
null <null>
2025-09-15 15:35:12 +0100

    upadtes apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './contexts/LanguageContext';
+import { AuthProvider } from './contexts/AuthContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
@@ -14,32 +15,50 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
+import AdminLogin from './pages/AdminLogin';
+import AdminDashboard from './pages/AdminDashboard';
+import AdminEventForm from './pages/AdminEventForm';
 
 function App() {
   return (
     <LanguageProvider>
-      <Router>
-        <ScrollToTop />
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="flex-grow">
+      <AuthProvider>
+        <Router>
+          <ScrollToTop />
+          <div className="flex flex-col min-h-screen">
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/events" element={<Events />} />
-              <Route path="/news" element={<News />} />
-              <Route path="/stories" element={<Stories />} />
-              <Route path="/stories/:storyId" element={<Stories />} />
-              <Route path="/discussions" element={<Discussions />} />
-              <Route path="/learning-hub" element={<LearningHub />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-              <Route path="/terms-of-service" element={<TermsOfService />} />
+              {/* Admin Routes */}
+              <Route path="/admin/login" element={<AdminLogin />} />
+              <Route path="/admin/dashboard" element={<AdminDashboard />} />
+              <Route path="/admin/events/new" element={<AdminEventForm />} />
+              
+              {/* Public Routes */}
+              <Route path="/*" element={
+                <>
+                  <Navbar />
+                  <main className="flex-grow">
+                    <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/events" element={<Events />} />
+                      <Route path="/news" element={<News />} />
+                      <Route path="/stories" element={<Stories />} />
+                      <Route path="/stories/:storyId" element={<Stories />} />
+                      <Route path="/discussions" element={<Discussions />} />
+                      <Route path="/services" element={<LearningHub />} />
+                      <Route path="/learning-hub" element={<LearningHub />} />
+                      <Route path="/about" element={<About />} />
+                      <Route path="/contact" element={<Contact />} />
+                      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                      <Route path="/terms-of-service" element={<TermsOfService />} />
+                    </Routes>
+                  </main>
+                  <Footer />
+                </>
+              } />
             </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+          </div>
+        </Router>
+      </AuthProvider>
     </LanguageProvider>
   );
 }
